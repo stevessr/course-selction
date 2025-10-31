cf71ee020b81a708a7d86eb4054fc38d0325f5be
stevessr <steve-ssr@outlook.com>
2025-10-31 20:17:36 +0800

    rebgein

--- backend/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-Entry point for running backend CLI as a module.
-Usage: python -m backend.cli [command] [args]
-"""
-
-if __name__ == '__main__':
-    from .cli import main
-    main()
-
--- ui-of-course-selection/src/views/NotFound.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>404 - Not Found</h1>
-  </div>
-</template>
-
-<script setup lang="ts">
-</script>
